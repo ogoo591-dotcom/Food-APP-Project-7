4cd958d0bf9129068721a5aa0a69eefddd018c5d
ogoo591-dotcom <ogoo591@gmail.com>
2025-11-28 17:48:10 +0800

    2025-11-28..

--- food-app-frontend/next.config.mjs
+++ food-app-frontend/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    PUBLIC_BACKEND_URL: process.env.PUBLIC_BACKEND_URL || "",
+  },
   images: {
     remotePatterns: [
       {
